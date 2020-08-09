77dd51a9418aa06a812c6487ceb5f90a1eeeffa5
Ingryd Thaiany Ravanelli Galon <67930165+IngrydR4v@users.noreply.github.com>
2020-08-09 10:01:16 -0300

    Add files via upload

---
+++ COVID-19/Covid19.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Covid19 {
+
+    //Informe a previsão de casos de infectados e de mortes de acordo com a quantidade de dias.
+
+    public static final double PERCENTUAL_MORTALIDADE = 0.02;
+
+    public static void Pandemia() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Há quantos dias começou a pandemia: ");
+        int dias = scanner.nextInt();
+
+        System.out.print("Qual o número de casos no momento: ");
+        int casos = scanner.nextInt();
+
+        double mostesAtuais = (casos * PERCENTUAL_MORTALIDADE);
+        System.out.println("Nº atual de mortes " + mostesAtuais);
+
+        double mediaDiaria = casos / dias;
+
+        double trintaDias = casos + (mediaDiaria * 30);
+        System.out.println("Nº previsto de casos para daqui 30 dias " + trintaDias);
+
+        double noventaDias = casos + (mediaDiaria * 90);
+        System.out.println("Nº previsto de casos para daqui 90 dias " + noventaDias);
+
+        double mortesNoventaDias = (noventaDias * PERCENTUAL_MORTALIDADE);
+
+        System.out.println("Nº de mortes para daqui 90 dias " + mortesNoventaDias);
+    }
+}
---
+++ COVID-19/Covid19_Principal.java
@@ -0,0 +1,5 @@
+public class Covid19_Principal {
+    public static void main(String[] args) {
+        Covid19.Pandemia();
+    }
+}
